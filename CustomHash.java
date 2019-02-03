20e0ba3c13afe48fbfd7535506c090b03088075a
Oleg <36641727+PlusqRED@users.noreply.github.com>
2019-02-03 12:41:21 +0300

    improve perfomance

--- CustomHash.java
+++ CustomHash.java
@@ -1,39 +1,37 @@
 import java.util.Random;
 
 public class CustomHash {
-    public static String getHash(String input, int hashSize) {
-        if(input.length() == 0) throw new IllegalArgumentException("input.length = 0");
-        else {
-            char[] chars = input.toCharArray();
-            long firstSeed = 1;
-            for (char aChar : chars) {
-                firstSeed *= aChar;
-            }
-            Random random = new Random(firstSeed);
-            long result = 0, a = 1;
-            for (char aChar : chars) {
-                result += aChar * a;
-                a += aChar + random.nextInt();
-            }
-            random.setSeed(result);
-            long hash = random.nextLong();
-            StringBuilder output = new StringBuilder(hashSize);
-            for(int i = 0; i < hashSize; i++) {
-                random.setSeed(hash + i + 1);
-                output.append(returnSymbol(hash + random.nextInt()));
-            }
-            return output.toString();
-        }
-
+    private CustomHash() {
     }
 
     private static Random random = new Random();
+
+    public static String getHash(String input, int hashSize) {
+        int hashsz = hashSize + 1;
+        char[] chars = input.toCharArray();
+        long result = 0, firstSeed = 1, a = 1;
+        for (char aChar : chars) {
+            result += aChar * a;
+            firstSeed *= aChar;
+            random.setSeed(firstSeed);
+            a += aChar + random.nextInt();
+        }
+        random.setSeed(result);
+        long hash = random.nextLong();
+        StringBuilder output = new StringBuilder(hashSize);
+        for (int i = 1; i < hashsz; ++i) {
+            random.setSeed(hash + i);
+            output.append(returnSymbol(hash + random.nextInt()));
+        }
+        return output.toString();
+    }
+
     private static char returnSymbol(long seed) {
         random.setSeed(seed);
         int randCase = random.nextInt(3);
-        if(randCase == 0) {
+        if (randCase == 0) {
             return (char) (random.nextInt(26) + 97);
-        } else if(randCase == 1) {
+        } else if (randCase == 1) {
             return (char) (random.nextInt(26) + 65);
         } else {
             return (char) (random.nextInt(10) + 48);
