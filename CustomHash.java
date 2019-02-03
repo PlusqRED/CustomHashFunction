c21b0c63dd00adea7a1658b3d2b27d05059c3fe6
Oleg <36641727+PlusqRED@users.noreply.github.com>
2019-02-03 17:27:51 +0300

    simplified

--- CustomHash.java
+++ CustomHash.java
@@ -8,7 +8,6 @@ public class CustomHash {
     private static String alphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789";
 
     public static String getHash(String input, int hashSize) {
-        int hashsz = hashSize + 1;
         char[] chars = input.toCharArray();
         random.setSeed(chars[0]);
         long a = random.nextLong();
@@ -21,7 +20,7 @@ public class CustomHash {
         random.setSeed(result);
         random.setSeed(random.nextLong());
         StringBuilder output = new StringBuilder(hashSize);
-        for (int i = 1; i < hashsz; ++i) {
+        for (int i = 0; i < hashSize; ++i) {
             output.append(alphabet.charAt(random.nextInt(62)));
         }
         return output.toString();
